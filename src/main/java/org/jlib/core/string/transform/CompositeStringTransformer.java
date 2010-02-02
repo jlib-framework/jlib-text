58499c4d8511427664a3db95149189490074a10e
igorakkerman <igorakkerman@88daaaa0-8d46-0410-88b0-ad3e7f9b0d50>
2010-02-02 10:49:58 +0000

    methods added: toString, equals, hashCode
    git-svn-id: https://jlib-api.svn.sourceforge.net/svnroot/jlib-api/jlib.core/trunk@185 88daaaa0-8d46-0410-88b0-ad3e7f9b0d50

--- src/main/java/org/jlib/core/string/transform/CompositeStringTransformer.java
+++ src/main/java/org/jlib/core/string/transform/CompositeStringTransformer.java
@@ -1,6 +1,7 @@
 package org.jlib.core.string.transform;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -23,19 +24,70 @@ implements StringTransformer {
      * specified {@link StringTransformer StringTransformers}.
      */
     public CompositeStringTransformer() {
-        stringTransformers = new LinkedList<StringTransformer>();
+        super();
+        stringTransformers = new ArrayList<StringTransformer>();
     }
 
     /**
      * Creates a new CompositeStringTransformer initially composed by the
-     * specified {@link StringTransformer StringTransformers}.
+     * {@link StringTransformer StringTransformers} specified in a {@link List}.
      * 
      * @param stringTransformers
      *        {@link List} of {@link StringTransformer StringTransformers}
-     *        composing this CompositeStringTransformer
+     *        initially composing this CompositeStringTransformer
      */
     public CompositeStringTransformer(List<? extends StringTransformer> stringTransformers) {
         this();
+        addStringTransformers(stringTransformers);
+    }
+
+    /**
+     * Creates a new CompositeStringTransformer initially composed by the
+     * {@link StringTransformer StringTransformers} specified in an argument
+     * list.
+     * 
+     * @param stringTransformers
+     *        argument list of {@link StringTransformer StringTransformers}
+     *        initially composing this CompositeStringTransformer
+     */
+    public CompositeStringTransformer(StringTransformer... stringTransformers) {
+        this();
+        addStringTransformers(stringTransformers);
+    }
+
+    /**
+     * Adds the specified {@link StringTransformer} as composing element of this
+     * CompositeStringTransformer.
+     * 
+     * @param stringTransformer
+     *        {@link StringTransformer} additionally composing this
+     *        CompositeStringTransformer
+     */
+    public void addStringTransformer(StringTransformer stringTransformer) {
+        stringTransformers.add(stringTransformer);
+    }
+
+    /**
+     * Adds the {@link StringTransformer StringTransformers} specified in an
+     * argument list as composing element of this CompositeStringTransformer.
+     * 
+     * @param stringTransformers
+     *        argument list of {@link StringTransformer StringTransformers}
+     *        additionally composing this CompositeStringTransformer
+     */
+    public void addStringTransformers(StringTransformer... stringTransformers) {
+        Collections.addAll(this.stringTransformers, stringTransformers);
+    }
+
+    /**
+     * Adds the {@link StringTransformer StringTransformers} specified in an
+     * argument list as composing elements of this CompositeStringTransformer.
+     * 
+     * @param stringTransformers
+     *        {@link List} of {@link StringTransformer StringTransformers}
+     *        additionally composing this CompositeStringTransformer
+     */
+    public void addStringTransformers(List<? extends StringTransformer> stringTransformers) {
         this.stringTransformers.addAll(stringTransformers);
     }
 
@@ -44,5 +96,34 @@ implements StringTransformer {
     public void transform(StringBuilder stringBuilder) {
         for (StringTransformer stringTransformer : stringTransformers)
             stringTransformer.transform(stringBuilder);
+    }
+
+    // @see java.lang.Object#toString()
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + stringTransformers;
+    }
+
+    /**
+     * Returns whether the specified {@link Object} is a
+     * CompositeStringTransformer composed by {@link StringTransformer
+     * StringTransformers} equal to those of this CompositeStringTransformer.
+     * 
+     * @param otherObject {@link Object} to compare to this CompositeStringTransformer
+     */
+    @Override
+    public boolean equals(Object otherObject) {
+        if (!(otherObject instanceof CompositeStringTransformer))
+            return false;
+        
+        CompositeStringTransformer otherCompositeStringTransformer = (CompositeStringTransformer) otherObject;
+        
+        return stringTransformers.equals(otherCompositeStringTransformer.stringTransformers);
+    }
+    
+    // @see java.lang.Object#hashCode()
+    @Override
+    public int hashCode() {
+        return stringTransformers.hashCode();
     }
 }
