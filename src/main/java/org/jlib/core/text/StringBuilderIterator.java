d3b453417e6af75dc05484679de44324e0c01a47
igorakkerman <igorakkerman@88daaaa0-8d46-0410-88b0-ad3e7f9b0d50>
2010-02-24 21:12:45 +0000

    CharSequenceIterator and StringBuilderIterator made usable for subsequences
    git-svn-id: https://jlib-api.svn.sourceforge.net/svnroot/jlib-api/jlib.core/trunk@232 88daaaa0-8d46-0410-88b0-ad3e7f9b0d50

--- src/main/java/org/jlib/core/text/StringBuilderIterator.java
+++ src/main/java/org/jlib/core/text/StringBuilderIterator.java
@@ -1,6 +1,7 @@
 package org.jlib.core.text;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Iterator over the {@link Character Characters} of a {@link StringBuilder}.
@@ -11,28 +12,13 @@ public class StringBuilderIterator
 extends CharSequenceIterator {
 
     /**
-     * Returns a new StringBuilderIterator over the {@link Character Characters}
-     * of the specified {@link StringBuilder}.
-     * 
-     * @param iteratedStringBuilder
-     *        {@link StringBuilder} to iterate
-     * @return new StringBuilderIterator over the {@link Character Characters}
-     *         of the specified {@link StringBuilder}
-     */
-    public static StringBuilderIterator iterator(StringBuilder iteratedStringBuilder) {
-        return new StringBuilderIterator(iteratedStringBuilder);
-    }
-
-    /**
-     * Returns an {@link Iterable} creating {@link StringBuilderIterator
-     * StringBuilderIterators} over the {@link Character Characters} of a
-     * {@link StringBuilder}.
+     * Returns an {@link Iterable} creating StringBuilderIterators over the
+     * {@link Character Characters} of a {@link StringBuilder}.
      * 
      * @param iterableStringBuilder
      *        {@link StringBuilder} to iterate
-     * @return {@link Iterable} creating {@link StringBuilderIterator
-     *         StringBuilderIterators} over the {@link Character Characters} of a
-     *         {@link StringBuilder}
+     * @return {@link Iterable} creating StringBuilderIterators over the
+     *         {@link Character Characters} of {@code iterableStringBuilder}
      */
     public static Iterable<Character> iterable(final StringBuilder iterableStringBuilder) {
         return new Iterable<Character>() {
@@ -44,7 +30,72 @@ extends CharSequenceIterator {
         };
     }
 
-    /** StringBuilder iterated by this StringBuilderIterator */
+    /**
+     * Returns an {@link Iterable} creating StringBuilderIterators over the
+     * {@link Character Characters} of the subsequence specified by the index of
+     * its first character (inclusive) contained by the specified
+     * {@link StringBuilder}.
+     * 
+     * @param iterableStringBuilder
+     *        {@link StringBuilder} to iterate
+     * @param firstCharacterIndex
+     *        integer specifying the index of the first character of the
+     *        subsequence
+     * @return {@link Iterable} creating StringBuilderIterators over the
+     *         {@link Character Characters} of the subsequence
+     * @throws CharSequenceBeginIndexNegativeException
+     *         if {@code firstCharacterIndex < 0}
+     * @throws CharSequenceBeginIndexAboveBoundException
+     *         if {@code firstCharacterIndex >= iteratedStringBuilder.length()}
+     */
+    public static Iterable<Character> iterable(final StringBuilder iterableStringBuilder, final int firstCharacterIndex)
+    throws CharSequenceBeginIndexNegativeException, CharSequenceBeginIndexAboveBoundException {
+        return new Iterable<Character>() {
+
+            @Override
+            public Iterator<Character> iterator() {
+                return new StringBuilderIterator(iterableStringBuilder, firstCharacterIndex);
+            }
+        };
+    }
+
+    /**
+     * Returns an {@link Iterable} creating StringBuilderIterators over the
+     * {@link Character Characters} of the subsequence specified by the indices
+     * of its first and last characters (inclusive) contained by the specified
+     * {@link StringBuilder}.
+     * 
+     * @param iterableStringBuilder
+     *        {@link StringBuilder} to iterate
+     * @param firstCharacterIndex
+     *        integer specifying the index of the first character of the
+     *        subsequence
+     * @param lastCharacterIndex
+     *        integer specifying the index of the last character of the
+     *        subsequence
+     * @return {@link Iterable} creating StringBuilderIterators over the
+     *         {@link Character Characters} of the subsequence
+     * @throws CharSequenceBeginIndexNegativeException
+     *         if {@code firstCharacterIndex < 0}
+     * @throws CharSequenceEndIndexBelowStartIndexException
+     *         if {@code lastCharacterIndex < firstCharacterIndex}
+     * @throws CharSequenceEndIndexAboveBoundException
+     *         if {@code lastCharacterIndex >= iteratedStringBuilder.length()}
+     */
+    public static Iterable<Character> iterable(final StringBuilder iterableStringBuilder, final int firstCharacterIndex,
+                                               final int lastCharacterIndex)
+    throws CharSequenceBeginIndexNegativeException, CharSequenceEndIndexBelowStartIndexException,
+    CharSequenceEndIndexAboveBoundException {
+        return new Iterable<Character>() {
+
+            @Override
+            public Iterator<Character> iterator() {
+                return new StringBuilderIterator(iterableStringBuilder, firstCharacterIndex, lastCharacterIndex);
+            }
+        };
+    }
+
+    /** {@link StringBuilder} iterated by this StringBuilderIterator */
     private final StringBuilder iteratedStringBuilder;
 
     /**
@@ -60,11 +111,61 @@ extends CharSequenceIterator {
     }
 
     /**
+     * Creates a new StringBuilderIterator over the {@link Character Characters}
+     * of the subsequence specified by the index of its first character
+     * (inclusive) contained by the specified {@link StringBuilder}.
+     * 
+     * @param iteratedStringBuilder
+     *        {@link StringBuilder} to iterate
+     * @param firstCharacterIndex
+     *        integer specifying the index of the first character of the
+     *        subsequence
+     * @throws CharSequenceBeginIndexNegativeException
+     *         if {@code firstCharacterIndex < 0}
+     * @throws CharSequenceBeginIndexAboveBoundException
+     *         if {@code firstCharacterIndex >= iteratedStringBuilder.length()}
+     */
+    public StringBuilderIterator(StringBuilder iteratedStringBuilder, int firstCharacterIndex) {
+        super(iteratedStringBuilder, firstCharacterIndex);
+        this.iteratedStringBuilder = iteratedStringBuilder;
+    }
+
+    /**
+     * Creates a new StringBuilderIterator over the {@link Character Characters}
+     * of the subsequence specified by the indices of its first and last
+     * characters (inclusive) contained by the specified {@link StringBuilder}.
+     * 
+     * @param iteratedStringBuilder
+     *        {@link StringBuilder} to iterate
+     * @param firstCharacterIndex
+     *        integer specifying the index of the first character of the
+     *        subsequence
+     * @param lastCharacterIndex
+     *        integer specifying the index of the last character of the
+     *        subsequence
+     * @throws CharSequenceBeginIndexNegativeException
+     *         if {@code firstCharacterIndex < 0}
+     * @throws CharSequenceEndIndexBelowStartIndexException
+     *         if {@code lastCharacterIndex < firstCharacterIndex}
+     * @throws CharSequenceEndIndexAboveBoundException
+     *         if {@code lastCharacterIndex >= iteratedStringBuilder.length()}
+     */
+    public StringBuilderIterator(StringBuilder iteratedStringBuilder, int firstCharacterIndex, int lastCharacterIndex)
+    throws CharSequenceBeginIndexNegativeException, CharSequenceEndIndexBelowStartIndexException,
+    CharSequenceEndIndexAboveBoundException {
+        super(iteratedStringBuilder, firstCharacterIndex, lastCharacterIndex);
+        this.iteratedStringBuilder = iteratedStringBuilder;
+    }
+
+    /**
      * Deletes the last {@link Character} returned by this StringBuilderIterator
      * from the {@link StringBuilder}.
      */
     @Override
     public void remove() {
+        if (nextCharacterIndex >= iteratedStringBuilder.length())
+            throw new NoSuchElementException("['" + iteratedStringBuilder + "', " + nextCharacterIndex + "]");
         iteratedStringBuilder.deleteCharAt(nextCharacterIndex - 1);
+        lastCharacterIndex --;
     }
 }
