2276bbf4d59b69c5d5e53ce1071f291f9fd0f444
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-04 12:16:52 +0200

    SequenceIteratorState created SingletonSequence implemented using SequenceIteratorState

---
+++ jlib.container/src/main/java/org/jlib/container/sequence/SequenceIteratorState.java
@@ -0,0 +1,27 @@
+package org.jlib.container.sequence;
+
+import org.jlib.core.iterator.IteratorState;
+
+/**
+ * State of a {@link SequenceIterator}.
+ * 
+ * @param <Element>
+ *        type of the elements held in the {@link Sequence}
+ * 
+ * @author Igor Akkerman
+ */
+public abstract class SequenceIteratorState<Element>
+extends IteratorState<Element>
+implements SequenceIterator<Element> {
+
+    /**
+     * Returns the previous {@link IteratorState}
+     * 
+     * @return previous {@link IteratorState}
+     */
+    public abstract SequenceIteratorState<Element> getPreviousState();
+
+    @Override
+    public abstract SequenceIteratorState<Element> getNextState();
+
+}
