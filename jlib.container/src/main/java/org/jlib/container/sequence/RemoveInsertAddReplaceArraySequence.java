8dbabeea98a4b7aabde3b0c79500d7ef394cbfd1
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-05 02:13:18 +0200

    RemoveSequence begun

---
+++ jlib.container/src/main/java/org/jlib/container/sequence/RemoveInsertAddReplaceArraySequence.java
@@ -0,0 +1,77 @@
+package org.jlib.container.sequence;
+
+import java.util.Collection;
+
+import org.jlib.container.Container;
+
+/**
+ * {@link AddReplaceArraySequence} into which Elements can be inserted.
+ * 
+ * @param <Element>
+ *        type of elements held in the {@link Sequence}
+ * 
+ * @author Igor Akkerman
+ */
+public class RemoveInsertAddReplaceArraySequence<Element>
+extends InsertAddReplaceArraySequence<Element>
+implements RemoveIndexSequence<Element> {
+
+    /**
+     * Creates a new {@link RemoveInsertAddReplaceArraySequence} with the
+     * specified first and last indices.
+     * 
+     * @param firstIndex
+     *        integer specifying the first index
+     * 
+     * @param lastIndex
+     *        integer specifying the last index
+     * 
+     * @throws IllegalArgumentException
+     *         if {@code lastIndex > firstIndex}
+     */
+    protected RemoveInsertAddReplaceArraySequence(final int firstIndex, final int lastIndex) {
+        super(firstIndex, lastIndex);
+    }
+
+    @Override
+    public RemoveInsertReplaceIndexSequenceIterator<Element> createIterator() {
+        return createIterator(getFirstIndex());
+    }
+
+    @Override
+    public RemoveInsertReplaceIndexSequenceIterator<Element> createIterator(final int startIndex)
+    throws SequenceIndexOutOfBoundsException {
+        return new DefaultRemoveInsertReplaceIndexSequenceIterator<Element>(this, startIndex);
+    }
+
+    @Override
+    public void remove(final Element element)
+    throws IllegalArgumentException {}
+
+    @Override
+    public void removeAll() {}
+
+    @Override
+    public void removeAll(final Container<? extends Element> elements) {}
+
+    @Override
+    public void removeAll(final Collection<? extends Element> elements) {}
+
+    @Override
+    public void removeAll(final Iterable<? extends Element> elements) {}
+
+    @Override
+    public void removeAll(final Element... elements) {}
+
+    @Override
+    public void retainAll(final Container<? extends Element> elements) {}
+
+    @Override
+    public void retainAll(final Collection<? extends Element> elements) {}
+
+    @Override
+    public void retainAll(final Element... elements) {}
+
+    @Override
+    public void remove(final int index) {}
+}
