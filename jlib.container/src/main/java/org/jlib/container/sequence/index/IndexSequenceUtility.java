0d52161e9bcec15b0447580957f07ddcd781d0f0
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-18 01:46:46 +0200

    createIntegerSequence methods moved to utility class

--- jlib.container/src/main/java/org/jlib/container/sequence/index/IndexSequenceUtility.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/index/IndexSequenceUtility.java
@@ -1,5 +1,7 @@
 package org.jlib.container.sequence.index;
 
+import org.jlib.container.sequence.Sequence;
+
 /**
  * {@link IndexSequence} utility.
  * 
@@ -73,4 +75,81 @@ public final class IndexSequenceUtility {
             throw new InvalidSequenceIndexRangeException(sequence, fromIndex, toIndex);
     }
 
+    // @formatter:off
+    /**
+     * <p>
+     * Creates a new {@link IndexSequence} containing the specified Integer
+     * Elements having a specified first index. That is, the index of the first
+     * Element of the specified sequence in the newly created
+     * {@link IndexSequence} can be specified. The fixed size of the newly
+     * created {@link IndexSequence} is the size of the specified sequence.
+     * </p>
+     * <p>
+     * It doesn't know whether the first parameter is meant to be the minimum
+     * index of the {@link IndexSequence} or the first Element of the sequence.
+     * You could pass an array of {@link Integer} values instead which is the
+     * equivalent to the sequence form for the argument
+     * {@code Integer... elements} but the newly created class provides an
+     * easier way: the factory methods
+     * {@link #createIntegerIndexSequence(IndexSequenceCreator, Integer...)} or
+     * {@link #createIntegerIndexSequenceFrom(IndexSequenceCreator, int, Integer[])}. The latter form takes
+     * the minimum index as first argument.
+     * </p>
+     * 
+     * {@literal
+     *     // possible but not handy
+     *     IndexSequence&lt;Integer&gt; integerSequence = createIntegerIndexSequence&lt;Integer&gt;new Integer[] 1, 2, 3, 4, 5, 6 });
+     * 
+     *     IndexSequence&lt;Integer&gt; integerSequence = createIntegerIndexSequence&lt;Integer&gt;(1, new Integer[] { 1, 2, 3, 4, 5, 6 });
+     * 
+     *     // easier to use (needs the static import of the factory method(s))
+     *     IndexSequence&lt;Integer&gt; integerSequence = createIntegerSequence(1, 2, 3, 4, 5);
+     * 
+     *     IndexSequence&lt;Integer&gt; integerSequence = createIntegerSequenceFrom(1, 1, 2, 3, 4, 5);
+     * }
+     * 
+     * @param <Sequenze>
+     *        type of the {@link InitializeableIndexSequence} created
+     * 
+     * @param indexSequenceCreator
+     *        {@link IndexSequenceCreator} to use
+     * 
+     * @param firstIndex
+     *        integer specifying the minimum index
+     * 
+     * @param elements
+     *        comma separated sequence of {@link Integer} Elements to store
+     * 
+     * @return new {@link IndexSequence} of {@link Integer} Elements
+     */
+    public static <Sequenze extends InitializeableIndexSequence<Integer>> 
+                  Sequenze createIntegerIndexSequenceFrom(final IndexSequenceCreator<Integer, Sequenze> indexSequenceCreator,
+                                                          final int firstIndex, final Integer... elements) {
+        // @formatter:on
+        return indexSequenceCreator.createSequence(firstIndex, elements);
+    }
+
+    /**
+     * Creates a new {@link Sequence} containing the specified Integer Elements
+     * having a first index of {@code 0}. The fixed size of the {@link Sequence}
+     * is the size of the specified sequence.
+     * 
+     * @param <Sequenze>
+     *        type of the {@link InitializeableIndexSequence} created
+     * 
+     * @param indexSequenceCreator
+     *        {@link IndexSequenceCreator} to use
+     * 
+     * @param elements
+     *        comma separated sequence of {@link Integer} elements to store
+     * 
+     * @return the newly created {@link Sequence}
+     */
+    // @formatter:off
+    public static <Sequenze extends InitializeableIndexSequence<Integer>> 
+                  Sequenze createIntegerIndexSequence(final IndexSequenceCreator<Integer, Sequenze> indexSequenceCreator,
+                                                      final Integer... elements) {
+        // @formatter:on
+        return createIntegerIndexSequenceFrom(indexSequenceCreator, 0, elements);
+    }
 }
