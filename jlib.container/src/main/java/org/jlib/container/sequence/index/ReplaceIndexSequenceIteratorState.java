adcff6160a9fc1a7ea7908b114cb051030e5bf78
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-24 16:30:09 +0200

    Replace state done

--- jlib.container/src/main/java/org/jlib/container/sequence/index/ReplaceIndexSequenceIteratorState.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/index/ReplaceIndexSequenceIteratorState.java
@@ -1,7 +1,16 @@
 package org.jlib.container.sequence.index;
 
 import org.jlib.container.sequence.ReplaceSequenceIteratorState;
+import org.jlib.container.sequence.Sequence;
 
+/**
+ * {@link IndexSequenceIteratorState} of a {@link ReplaceIndexSequence}.
+ * 
+ * @param <Element>
+ *        type of elements held in the {@link Sequence}
+ * 
+ * @author Igor Akkerman
+ */
 public interface ReplaceIndexSequenceIteratorState<Element>
 extends IndexSequenceIteratorState<Element>, ReplaceSequenceIteratorState<Element> {
     // unifying
