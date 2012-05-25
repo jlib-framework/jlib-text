32b0bb1663d25ff3c0c8e0bfd798c8de26da5e6c
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-25 05:52:03 +0200

    Revert "InsertIndexSequenceIterator: getSequence method added"
    This reverts commit 798d891bed190cbb4bc0b5bc023541c3e4faadde.

--- jlib.container/src/main/java/org/jlib/container/sequence/index/InsertIndexSequenceIterator.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/index/InsertIndexSequenceIterator.java
@@ -5,7 +5,7 @@ import org.jlib.container.sequence.Sequence;
 import org.jlib.container.sequence.SequenceIterator;
 
 /**
- * {@link SequenceIterator} over the Elements of an {@link InsertIndexSequence}.
+ * {@link SequenceIterator} over the elements of an {@link InsertIndexSequence}.
  * 
  * @param <Element>
  *        type of elements held in the {@link Sequence}
@@ -14,12 +14,5 @@ import org.jlib.container.sequence.SequenceIterator;
  */
 public interface InsertIndexSequenceIterator<Element>
 extends InsertSequenceIterator<Element>, IndexSequenceIterator<Element> {
-
-    /**
-     * Returns the traversed {@link InsertIndexSequence}
-     * 
-     * @return traversed {@link InsertIndexSequence}
-     */
-    @Override
-    public InsertIndexSequence<Element> getSequence();
+    // unifying interface to satisfy the Eclipse compiler
 }
