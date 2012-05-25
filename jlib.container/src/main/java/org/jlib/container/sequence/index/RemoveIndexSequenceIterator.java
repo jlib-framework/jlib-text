b694a7b5752f4b129c58d42eac29315d42a073e9
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-25 05:04:17 +0200

    RemoveIndexSequenceIterator: getSequence method added

--- jlib.container/src/main/java/org/jlib/container/sequence/index/RemoveIndexSequenceIterator.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/index/RemoveIndexSequenceIterator.java
@@ -1,9 +1,25 @@
 package org.jlib.container.sequence.index;
 
+import org.jlib.container.sequence.RemoveSequence;
 import org.jlib.container.sequence.RemoveSequenceIterator;
+import org.jlib.container.sequence.Sequence;
 
-@SuppressWarnings("javadoc")
-public interface RemoveIndexSequenceIterator<RemoveIndexSequenceElement>
-extends RemoveSequenceIterator<RemoveIndexSequenceElement>, IndexSequenceIterator<RemoveIndexSequenceElement> {
-// unifying interface to satisfy the Eclipse compiler
+/**
+ * {@link IndexSequenceIterator} traversing a {@link RemoveSequence}.
+ * 
+ * @param <Element>
+ *        type of elements held in the {@link Sequence}
+ * 
+ * @author Igor Akkerman
+ */
+public interface RemoveIndexSequenceIterator<Element>
+extends RemoveSequenceIterator<Element>, IndexSequenceIterator<Element> {
+
+    /**
+     * Returns the traversed {@link RemoveIndexSequence}
+     * 
+     * @return traversed {@link RemoveIndexSequence}
+     */
+    @Override
+    public RemoveIndexSequence<Element> getSequence();
 }
