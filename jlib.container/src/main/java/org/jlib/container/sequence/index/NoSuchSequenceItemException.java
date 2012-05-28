859c5f2abafce62be610018fa37770230b393b4c
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-28 13:44:19 +0200

    NoSuchSequenceItemException: item property created

--- jlib.container/src/main/java/org/jlib/container/sequence/index/NoSuchSequenceItemException.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/index/NoSuchSequenceItemException.java
@@ -1,5 +1,7 @@
 package org.jlib.container.sequence.index;
 
+import java.text.MessageFormat;
+
 import org.jlib.container.sequence.Sequence;
 import org.jlib.container.sequence.SequenceException;
 
@@ -14,6 +16,9 @@ extends SequenceException {
     /** serialVersionUID */
     private static final long serialVersionUID = -8162511917404174346L;
 
+    /** Item that could not be found */
+    private final Object item;
+
     /**
      * Creates a new {@link NoSuchSequenceItemException}.
      * 
@@ -27,6 +32,17 @@ extends SequenceException {
      *        Item that could not be found
      */
     public <Item> NoSuchSequenceItemException(final Sequence<Item> sequence, final Item item) {
-        super(sequence);
+        super(sequence, MessageFormat.format("{1}: {2}", item, sequence));
+
+        this.item = item;
+    }
+
+    /**
+     * Returns the item of this {@link NoSuchSequenceItemException}.
+     * 
+     * @return {@link Object} specifying the item
+     */
+    public Object getItem() {
+        return item;
     }
 }
