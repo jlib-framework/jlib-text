62688b22d3fc8302e65906054cb93c6908c92866
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-28 13:34:33 +0200

    SequenceException: multiple constructors created

--- jlib.container/src/main/java/org/jlib/container/sequence/SequenceException.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/SequenceException.java
@@ -1,5 +1,7 @@
 package org.jlib.container.sequence;
 
+import java.text.MessageFormat;
+
 /**
  * {@link RuntimeException} referencing a {@link Sequence}.
  * 
@@ -9,7 +11,7 @@ public abstract class SequenceException
 extends RuntimeException {
 
     /** serialVersionUID */
-    private static final long serialVersionUID = -5009055546803096622L;
+    private static final long serialVersionUID = -540223071568730842L;
 
     /** referenced {@link Sequence} */
     private final Sequence<?> sequence;
@@ -21,7 +23,50 @@ extends RuntimeException {
      *        referenced {@link Sequence}
      */
     public SequenceException(final Sequence<?> sequence) {
-        super();
+        this(sequence, (Throwable) null);
+    }
+
+    /**
+     * Creates a new {@link SequenceException} with the specified error message.
+     * 
+     * @param sequence
+     *        referenced {@link Sequence}
+     * 
+     * @param errorMessage
+     *        {@link String} specifying the error message
+     */
+    public SequenceException(final Sequence<?> sequence, final String errorMessage) {
+        this(sequence, errorMessage, null);
+    }
+
+    /**
+     * Creates a new {@link SequenceException} with the specified cause.
+     * 
+     * @param sequence
+     *        referenced {@link Sequence}
+     * 
+     * @param cause
+     *        {@link Throwable} that caused this {@link SequenceException}
+     */
+    public SequenceException(final Sequence<?> sequence, final Throwable cause) {
+        this(sequence, "{1}", cause);
+    }
+
+    /**
+     * Creates a new {@link SequenceException} with the specified error message
+     * and cause.
+     * 
+     * @param sequence
+     *        referenced {@link Sequence}
+     * 
+     * @param errorMessage
+     *        {@link String} specifying the error message
+     * 
+     * @param cause
+     *        {@link Throwable} that caused this {@link SequenceException}
+     */
+    public SequenceException(final Sequence<?> sequence, final String errorMessage, final Throwable cause) {
+        super(MessageFormat.format(errorMessage, sequence), cause);
 
         this.sequence = sequence;
     }
