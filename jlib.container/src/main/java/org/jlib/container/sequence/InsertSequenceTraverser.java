62aae937a2fb483524fa15186becac00645c245a
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-31 07:13:34 +0200

    InsertSequenceTraverser: Javadoc and signature optimized

--- jlib.container/src/main/java/org/jlib/container/sequence/InsertSequenceTraverser.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/InsertSequenceTraverser.java
@@ -14,9 +14,10 @@
 
 package org.jlib.container.sequence;
 
+import org.jlib.container.sequence.index.IndexSequenceTraverser;
 
 /**
- * Traverser over a {@link InsertSequence}.
+ * {@link SequenceTraverser} over a {@link InsertSequence}.
  * 
  * @param <Item>
  *        type of items held in the {@link Sequence}
@@ -28,26 +29,28 @@ extends SequenceTraverser<Item> {
 
     /**
      * <p>
-     * Inserts the specified Item into the sequence at the current position
-     * of this Traverser.
+     * Inserts the specified Item into the sequence at the current position of
+     * this {@link IndexSequenceTraverser}.
      * </p>
      * <p>
-     * The Item is inserted immediately before the next Item that would
-     * have been returned by {@link #getNextItem()} and immediately after the previous
-     * Item that would have been returned by {@link #getPreviousItem()}.
+     * The Item is inserted immediately before the next Item that would have
+     * been returned by {@link #getNextItem()} and immediately after the
+     * previous Item that would have been returned by {@link #getPreviousItem()}
+     * .
      * </p>
      * <p>
      * A subsequent call to {@link #getNextItem()} would be unaffected, and a
      * subsequent call to {@link #getPreviousItem()} would return the new item.
      * </p>
      * 
-     * @param item
-     *        Item to insert
+     * @param newItem
+     *        new Item to insert
      * 
      * @throws IllegalStateException
-     *         if {@link #getNextItem()} or {@link #getPreviousItem()} have not been called
-     *         initially or after the last call to {@link #insert(Object)}
+     *         if {@link #getNextItem()} or {@link #getPreviousItem()} have not
+     *         been called initially or after the last call to
+     *         {@link #insert(Object)}
      */
-    public void insert(Item item)
+    public void insert(final Item newItem)
     throws IllegalStateException;
 }
