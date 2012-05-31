285db0eaec9c37a9f029e72b0830312a6bc8ca2b
Igor Akkerman <igor.akkerman@jlib.org>
2012-05-31 16:03:22 +0200

    CompleteArraySequence created

---
+++ jlib.container/src/main/java/org/jlib/container/sequence/index/array/CompleteArraySequence.java
@@ -0,0 +1,28 @@
+package org.jlib.container.sequence.index.array;
+
+import org.jlib.container.sequence.Sequence;
+
+/**
+ * Full-featured {@link ArraySequence} providing the most of capabilities.
+ * 
+ * @param <Item>
+ *        type of the items held in the {@link Sequence}
+ * 
+ * @author Igor Akkerman
+ */
+public class CompleteArraySequence<Item>
+extends InsertEndsRemoveAddReplaceArraySequence<Item> {
+
+    /**
+     * Creates a new {@link CompleteArraySequence}.
+     * 
+     * @param firstIndex
+     *        first index of this {@link CompleteArraySequence}
+     * 
+     * @param lastIndex
+     *        last index of this {@link CompleteArraySequence}
+     */
+    protected CompleteArraySequence(final int firstIndex, final int lastIndex) {
+        super(firstIndex, lastIndex);
+    }
+}
