f349362822ebd63540f78797623fea2a014449b7
Igor Akkerman <igor.akkerman@jlib.org>
2012-06-06 01:24:18 +0200

    ObservedHeadRemoveSequence: created

--- jlib.container/src/main/java/org/jlib/container/sequence/ObservedHeadRemoveSequence.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/ObservedHeadRemoveSequence.java
@@ -1,9 +1,11 @@
 package org.jlib.container.sequence;
 
 import org.jlib.core.observer.ItemObserver;
+import org.jlib.core.observer.ItemObserverException;
 
 /**
- * {@link Sequence} allowing its head Item to be removed.
+ * {@link Sequence} allowing its Items to be traversed using a
+ * {@link RemoveSequenceTraverser}.
  * 
  * @param <Item>
  *        type of the items held in the {@link Sequence}
@@ -19,9 +21,13 @@ extends HeadRemoveSequence<Item> {
      * @param observers
      *        comma separated sequence of {@link ItemObserver} instances
      *        attending the removal
+     * 
+     * @throws ItemObserverException
+     *         if an error occurs during the {@link ItemObserver} operation
      */
     // @formatter:off
     public void removeFirstItem(@SuppressWarnings({ "unchecked", /* "varargs" */}) 
-                                final ItemObserver<Item>... observers);
+                                final ItemObserver<Item>... observers)
+    throws ItemObserverException;
     // @formatter:on
 }
