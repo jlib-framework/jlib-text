7831a98cc6e29b59d8aab397e56be8e1d51f6f3b
Igor Akkerman <igor.akkerman@jlib.org>
2012-06-09 01:50:16 +0200

    IteratorUtility with getItemsCount method created

---
+++ jlib.core/src/main/java/org/jlib/core/iterator/IteratorUtility.java
@@ -0,0 +1,35 @@
+package org.jlib.core.iterator;
+
+import java.util.Iterator;
+
+/**
+ * {@link Iterator} utility.
+ * 
+ * @author Igor Akkerman
+ */
+public final class IteratorUtility {
+
+    /** no visible constructor */
+    private IteratorUtility() {}
+
+    /**
+     * Returns the number of Items provided by the specified {@link Iterable}.
+     * 
+     * @param items
+     *        {@link Iterator} providing the items
+     * 
+     * @return integer specifying the number of Items
+     */
+    public static int getItemsCount(final Iterable<?> items) {
+        int itemsCount = 0;
+
+        // @formatter:off
+        for (@SuppressWarnings("unused")
+                 final Object item : items)
+            itemsCount ++;
+
+        return itemsCount;
+
+        // @formatter:on
+    }
+}
