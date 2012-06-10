ad159160c57b30283464cf4a2e4a4d6cac003dc9
Igor Akkerman <igor.akkerman@jlib.org>
2012-06-10 15:49:45 +0200

    ArrayUtility: getItemsCount modified

--- jlib.core/src/main/java/org/jlib/core/array/ArrayUtility.java
+++ jlib.core/src/main/java/org/jlib/core/array/ArrayUtility.java
@@ -25,4 +25,24 @@ public final class ArrayUtility {
     public static <Item> Iterable<Item> iterable(final Item... items) {
         return new ArrayIterable<>(items);
     }
+
+    /**
+     * Returns the total number of non array items held in the specified array,
+     * recursively descending in every array item.
+     * 
+     * @param items
+     *        comma separated sequence of {@link Object} items
+     * 
+     * @return integer specifying the total number of items
+     */
+    public static int getItemsCount(final Object... items) {
+        int itemsCount = 0;
+
+        for (final Object item : items)
+            itemsCount += item.getClass().isArray()
+                ? getItemsCount((Object[]) item)
+                : 1;
+
+        return itemsCount;
+    }
 }
