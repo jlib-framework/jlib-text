aef3e233f23b96add6ffc3bf4ff9476fb86efb44
Igor Akkerman <igor.akkerman@jlib.org>
2012-07-10 11:38:52 +0200

    IteratorUtility and TraverserUtility: provideEqualItems added

--- jlib.core/src/main/java/org/jlib/core/traverser/TraverserUtility.java
+++ jlib.core/src/main/java/org/jlib/core/traverser/TraverserUtility.java
@@ -1,5 +1,7 @@
 package org.jlib.core.traverser;
 
+import org.jlib.core.ObjectUtility;
+
 /**
  * Utility providing operations on {@link Traverser} and {@link Traversible}
  * instances.
@@ -10,6 +12,46 @@ public final class TraverserUtility {
 
     /** no visible constructor */
     private TraverserUtility() {}
+
+    /**
+     * Verifies whether the {@link Traverser} instances created by the
+     * {@link Traversible#createTraverser()} method of the two specified
+     * {@link Traversible} instances traverse the same number of Items and all
+     * traversed Items are equal. Two Items {@code item1} and {@code item2} are
+     * called equal if {@code item1.equals(item2)}.
+     * 
+     * @param <Item>
+     *        type of the items traversed by {@code traversible1} and
+     *        {@code traversible2}
+     * 
+     * @param traversible1
+     *        first traversed {@link Traversible}
+     * 
+     * @param traversible2
+     *        second traversed {@link Traversible}
+     * 
+     * @return {@code true} if {@code traverser1} and {@code traverser2} provide
+     *         equal Items; {@code false} otherwise
+     */
+    public static <Item> boolean provideEqualItems(final Traversible<Item> traversible1,
+                                                   final Traversible<Item> traversible2) {
+        final Traverser<?> traverser1 = traversible1.createTraverser();
+        final Traverser<?> traverser2 = traversible2.createTraverser();
+
+        do {
+            final boolean nextItemAccessible = traverser1.isNextItemAccessible();
+
+            if (nextItemAccessible != traverser2.isNextItemAccessible())
+                return false;
+
+            if (!nextItemAccessible)
+                return true;
+
+            if (!ObjectUtility.equal(traverser1.getNextItem(), traverser2.getNextItem()))
+                return false;
+        }
+        while (true);
+    }
 
     /**
      * Returns the number of Items provided by the specified
