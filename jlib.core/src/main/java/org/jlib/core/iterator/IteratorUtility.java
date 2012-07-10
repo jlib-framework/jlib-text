415f4e85e95ab60ffe8613e9df34b00afb017d64
Igor Akkerman <Igor Akkerman@hottie.home.igor>
2012-07-10 11:38:52 +0200

    IteratorUtility and TraverserUtility: provideEqualItems added

--- jlib.core/src/main/java/org/jlib/core/iterator/IteratorUtility.java
+++ jlib.core/src/main/java/org/jlib/core/iterator/IteratorUtility.java
@@ -13,6 +13,45 @@ public final class IteratorUtility {
     private IteratorUtility() {}
 
     /**
+     * Verifies whether the {@link Iterator} instances created by the
+     * {@link Iterable#iterator()} method of the two specified {@link Iterable}
+     * instances traverse the same number of Items and all traversed Items are
+     * equal. Two Items {@code item1} and {@code item2} are called equal if
+     * {@code item1.equals(item2)}.
+     * 
+     * @param <Item>
+     *        type of the items traversed by {@code iterable1} and
+     *        {@code iterable2}
+     * 
+     * @param iterable1
+     *        first traversed {@link Iterable}
+     * 
+     * @param iterable2
+     *        second traversed {@link Iterable}
+     * 
+     * @return {@code true} if {@code iterator1} and {@code iterator2} provide
+     *         equal Items; {@code false} otherwise
+     */
+    public static <Item> boolean provideEqualItems(final Iterable<Item> iterable1, final Iterable<Item> iterable2) {
+        final Iterator<?> iterator1 = iterable1.iterator();
+        final Iterator<?> iterator2 = iterable2.iterator();
+
+        do {
+            final boolean nextItemAccessible = iterator1.hasNext();
+
+            if (nextItemAccessible != iterator2.hasNext())
+                return false;
+
+            if (!nextItemAccessible)
+                return true;
+
+            if (!iterator1.next().equals(iterator2.next()))
+                return false;
+        }
+        while (true);
+    }
+
+    /**
      * Returns the number of Items provided by the specified {@link Iterable}.
      * 
      * @param items
