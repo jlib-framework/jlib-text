07e3205b78682c9e97edb5a2aed3c8390040ed8b
Igor Akkerman <igor.akkerman@jlib.org>
2012-07-19 15:37:25 +0200

    LinearIndexStorageCapacityStrategy: assertCapacity method without location (head/tail/middle) removed

--- jlib.container/src/main/java/org/jlib/container/sequence/index/array/storage/LinearIndexStorageCapacityStrategy.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/index/array/storage/LinearIndexStorageCapacityStrategy.java
@@ -8,15 +8,6 @@ package org.jlib.container.sequence.index.array.storage;
 public interface LinearIndexStorageCapacityStrategy {
 
     /**
-     * Asserts that the {@link LinearIndexStorage} fits the specified number of
-     * Items.
-     * 
-     * @param itemsCount
-     *        integer specifying the number of Items
-     */
-    public void assertCapacity(int itemsCount);
-
-    /**
      * Asserts that the referenced {@link LinearIndexStorage} fits the specified
      * number of Items, leaving a hole of the specified size between the
      * existing Items. If necessary, the Items located behind the hole index are
