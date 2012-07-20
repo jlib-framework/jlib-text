c0f8aa97166883dd8c25832e87f84f078d4be98f
Igor Akkerman <igor.akkerman@jlib.org>
2012-07-21 01:07:42 +0200

    MathUtility created

---
+++ jlib.core/src/main/java/org/jlib/core/math/MathUtility.java
@@ -0,0 +1,28 @@
+package org.jlib.core.math;
+
+/**
+ * Utility providing mathematical operations.
+ * 
+ * @author Igor Akkerman
+ */
+public final class MathUtility {
+
+    /** no visible constructor */
+    private MathUtility() {}
+
+    /**
+     * Returns the number of numbers between a minimum and a maximum number,
+     * both inclusive.
+     * 
+     * @param minimum
+     *        integer specifying the minimum number
+     * 
+     * @param maximum
+     *        integer specifying the maximum number
+     * 
+     * @return {@code maximum - minimum + 1}
+     */
+    public int count(final int minimum, final int maximum) {
+        return maximum - minimum + 1;
+    }
+}
