78338f5c067fd13c97219529f6f94e5e73d4f195
Igor Akkerman <igor.akkerman@jlib.org>
2012-07-28 18:16:17 +0200

    double import removed

--- jlib.container/src/main/java/org/jlib/container/sequence/index/ObservedReplaceInsertRemoveIndexSequence.java
+++ jlib.container/src/main/java/org/jlib/container/sequence/index/ObservedReplaceInsertRemoveIndexSequence.java
@@ -27,9 +27,10 @@ import org.jlib.container.sequence.Sequence;
  * @author Igor Akkerman
  */
 public interface ObservedReplaceInsertRemoveIndexSequence<Item>
-extends ObservedReplaceInsertIndexSequence<Item>, ObservedRemoveIndexSequence<Item>,
-ObservedReplaceInsertRemoveFirstIndexSequence<Item>, ObservedReplaceInsertRemoveLastIndexSequence<Item>,
-ObservedReplaceAppendRemoveSequence<Item> {
+extends          ObservedRemoveIndexSequence<Item>,
+                 ObservedReplaceInsertRemoveFirstIndexSequence<Item>, 
+                 ObservedReplaceInsertRemoveLastIndexSequence<Item>,
+                 ObservedReplaceAppendRemoveSequence<Item> {
 
     @Override
     public ObservedReplaceInsertRemoveIndexSequence<Item> getSubsequenceView(int fromIndex, int toIndex)
