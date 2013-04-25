71ea547637c7194ed21fad61c3c0048f524cb7b6
Igor Akkerman <igor.akkerman@dynamite-services.de>
2013-04-25 14:42:56 +0200

    equal: final modifier removed

--- jlib.core/src/main/java/org/jlib/core/ObjectUtility.java
+++ jlib.core/src/main/java/org/jlib/core/ObjectUtility.java
@@ -1,31 +1,31 @@
 /*******************************************************************************
- * 
+ *
  *    jlib - Open Source Java Library
- * 
+ *
  *    www.jlib.org
- * 
- * 
+ *
+ *
  *    Copyright 2012 Igor Akkerman
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
- * 
+ *
  ******************************************************************************/
 
 package org.jlib.core;
 
 /**
  * Utility class providing static methods for Object operations.
- * 
+ *
  * @author Igor Akkerman
  */
 public final class ObjectUtility {
@@ -35,20 +35,14 @@ public final class ObjectUtility {
 
     /**
      * Returns the specified optional Value.
-     * 
-     * @param <Value>
-     *        type of the value
-     * 
-     * @param valueName
-     *        {@link String} specifying a descriptive name of the Value
-     * 
-     * @param value
-     *        Value to return or {@code null}
-     * 
+     *
+     * @param <Value>   type of the value
+     * @param valueName {@link String} specifying a descriptive name of the Value
+     * @param value     Value to return or {@code null}
+     *
      * @return {@code value}, if {@code value != null}
-     * 
-     * @throws ValueNotAccessibleException
-     *         if {@code value == null}
+     *
+     * @throws ValueNotAccessibleException if {@code value == null}
      */
     public static <Value> Value optional(final String valueName, /* @Nullable */final Value value)
     throws ValueNotAccessibleException {
@@ -62,24 +56,23 @@ public final class ObjectUtility {
      * Compares the specified Objects for mutual equality. Two Objects
      * {@code object1}, {@code object2} are considered equal if
      * {@code object1 == object2 == null} or {@code object1.equals(object2)}.
-     * 
-     * @param objects
-     *        comma separated sequence of Objects to compare
-     * 
+     *
+     * @param objects comma separated sequence of Objects to compare
+     *
      * @return {@code true} if all specified Objects are equal or if the
      *         specified sequence of Objects is empty; {@code false} otherwise
      */
-    public static final boolean equal(final Object... objects) {
+    public static boolean equal(final Object... objects) {
         if (objects.length == 0)
             return true;
         final Object firstObject = objects[0];
         if (firstObject != null) {
-            for (int index = 1; index < objects.length; index ++)
-                if (!firstObject.equals(objects[index]))
+            for (int index = 1; index < objects.length; index++)
+                if (! firstObject.equals(objects[index]))
                     return false;
         }
         else
-            for (int index = 1; index < objects.length; index ++)
+            for (int index = 1; index < objects.length; index++)
                 if (objects[index] != null)
                     return false;
         return true;
@@ -89,15 +82,15 @@ public final class ObjectUtility {
      * Returns the hash code of the specified Object as returned by its
      * {@link Object#hashCode() hashCode} method or {@code 0} if the specified
      * Object is {@code null}.
-     * 
-     * @param object
-     *        Object of which the hash code is returned
+     *
+     * @param object Object of which the hash code is returned
+     *
      * @return integer specifying the hash code of Object; {@code 0} if
      *         {@code object} is {@code null}
      */
     public static int hashCode(final Object object) {
         return object != null
-            ? object.hashCode()
-            : 0;
+               ? object.hashCode()
+               : 0;
     }
 }
