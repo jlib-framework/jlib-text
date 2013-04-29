c9bd06bbcee05cd5716e7629b266cdfc9c0e637f
Igor Akkerman <igor.akkerman@dynamite-services.de>
2013-04-30 01:53:01 +0200

    formatted

--- jlib-core/src/main/java/org/jlib/core/traverser/TraverserUtility.java
+++ jlib-core/src/main/java/org/jlib/core/traverser/TraverserUtility.java
@@ -1,24 +1,24 @@
 /*******************************************************************************
- * 
+ *
  *    jlib - Open Source Java Library
- * 
+ *
  *    www.jlib.org
- * 
- * 
+ *
+ *
  *    Copyright 2012 Igor Akkerman
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
- * 
+ *
  ******************************************************************************/
 
 package org.jlib.core.traverser;
@@ -26,9 +26,8 @@ package org.jlib.core.traverser;
 import org.jlib.core.ObjectUtility;
 
 /**
- * Utility providing operations on {@link Traverser} and {@link Traversible}
- * instances.
- * 
+ * Utility providing operations on {@link Traverser} and {@link Traversible} instances.
+ *
  * @author Igor Akkerman
  */
 public final class TraverserUtility {
@@ -37,27 +36,21 @@ public final class TraverserUtility {
     private TraverserUtility() {}
 
     /**
-     * Verifies whether the {@link Traverser} instances created by the
-     * {@link Traversible#createTraverser()} methods of the two specified
-     * {@link Traversible} instances traverse the same number of Items in the
-     * same order and all traversed Items are equal. Two Items {@code item1} and
-     * {@code item2} are called equal if {@code item1.equals(item2)}.
-     * 
+     * Verifies whether the {@link Traverser} instances created by the {@link Traversible#createTraverser()} methods of
+     * the two specified {@link Traversible} instances traverse the same number of Items in the same order and all
+     * traversed Items are equal. Two Items {@code item1} and {@code item2} are called equal if {@code
+     * item1.equals(item2)}.
+     *
      * @param <Item>
-     *        type of the items traversed by {@code traversible1} and
-     *        {@code traversible2}
-     * 
+     * type of the items traversed by {@code traversible1} and {@code traversible2}
      * @param traversible1
-     *        first traversed {@link Traversible}
-     * 
+     * first traversed {@link Traversible}
      * @param traversible2
-     *        second traversed {@link Traversible}
-     * 
-     * @return {@code true} if {@code traverser1} and {@code traverser2} provide
-     *         equal Items; {@code false} otherwise
+     * second traversed {@link Traversible}
+     *
+     * @return {@code true} if {@code traverser1} and {@code traverser2} provide equal Items; {@code false} otherwise
      */
-    public static <Item> boolean provideEqualItems(final Traversible<Item> traversible1,
-                                                   final Traversible<Item> traversible2) {
+    public static <Item> boolean provideEqualItems(final Traversible<Item> traversible1, final Traversible<Item> traversible2) {
         final Traverser<?> traverser1 = traversible1.createTraverser();
         final Traverser<?> traverser2 = traversible2.createTraverser();
 
@@ -67,22 +60,21 @@ public final class TraverserUtility {
             if (nextItemAccessible != traverser2.isNextItemAccessible())
                 return false;
 
-            if (!nextItemAccessible)
+            if (! nextItemAccessible)
                 return true;
 
-            if (!ObjectUtility.equal(traverser1.getNextItem(), traverser2.getNextItem()))
+            if (! ObjectUtility.equal(traverser1.getNextItem(), traverser2.getNextItem()))
                 return false;
         }
         while (true);
     }
 
     /**
-     * Returns the number of Items provided by the specified
-     * {@link TwoWayTraversible}.
-     * 
+     * Returns the number of Items provided by the specified {@link TwoWayTraversible}.
+     *
      * @param traversible
-     *        {@link TwoWayTraversible} providing the items
-     * 
+     * {@link TwoWayTraversible} providing the items
+     *
      * @return integer specifying the number of Items
      */
     public static int getItemsCount(final TwoWayTraversible<?> traversible) {
@@ -93,20 +85,18 @@ public final class TraverserUtility {
         while (itemsTraverser.isNextItemAccessible()) {
             itemsTraverser.getNextItem();
 
-            itemsCount ++;
+            itemsCount++;
         }
 
         return itemsCount;
     }
 
     /**
-     * Returns the sum of number of Items provided by the specified
-     * {@link TwoWayTraverser} instances.
-     * 
+     * Returns the sum of number of Items provided by the specified {@link TwoWayTraverser} instances.
+     *
      * @param traversibles
-     *        comma separated sequence of {@link TwoWayTraversible}
-     *        instances
-     * 
+     * comma separated sequence of {@link TwoWayTraversible} instances
+     *
      * @return integer specifying the number of Items
      */
     public static int getItemsCount(final TwoWayTraversible<?>... traversibles) {
