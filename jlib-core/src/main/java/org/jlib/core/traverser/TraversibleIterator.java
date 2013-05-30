9d52ff770d5d10df9e8991932ae5e228dc700782
Igor Akkerman <igor.akkerman@dynamite-services.de>
2013-05-30 16:37:59 +0200

    TraversibleIterator: Javadoc comment optimized

--- jlib-core/src/main/java/org/jlib/core/traverser/TraversibleIterator.java
+++ jlib-core/src/main/java/org/jlib/core/traverser/TraversibleIterator.java
@@ -1,41 +1,41 @@
 /*******************************************************************************
- * 
+ *
  *    jlib - Open Source Java Library
- * 
+ *
  *    www.jlib.org
- * 
- * 
+ *
+ *
  *    Copyright 2012 Igor Akkerman
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
- * 
+ *
  ******************************************************************************/
 
 package org.jlib.core.traverser;
 
-import java.util.Iterator;
-
 import org.jlib.core.iterator.AbstractIterator;
+
+import java.util.Iterator;
 
 /**
  * {@link Iterator} over the Items of a {@link Traversible}.
- * 
+ *
  * @param <Item>
  *        type of items of the {@link Traversible}
- * 
+ *
  * @param <Travble>
  *        type of the traversed {@link Traversible}
- * 
+ *
  * @author Igor Akkerman
  */
 public class TraversibleIterator<Item, Travble extends Traversible<Item>>
@@ -46,7 +46,7 @@ extends AbstractIterator<Item> {
 
     /**
      * Creates a new {@link TraversibleIterator}.
-     * 
+     *
      * @param traversible
      *        traversed {@link Traversible}
      */
@@ -58,15 +58,15 @@ extends AbstractIterator<Item> {
 
     /**
      * <p>
-     * Registers a newly createTraverser} for the specified
-     * {@link Traversible} as delegate for this {@link TraversibleIterator}.
+     * Registers a newly created delegate {@link Traverser} for the specified
+     * {@link Traversible}.
      * </p>
      * <p>
-     * This method mmust be overridden by subclasses using parametric
+     * This method must be overridden by subclasses using parametric
      * polymorphism to register the concrete {@link Traverser} in a property of
      * the concrete type.
      * </p>
-     * 
+     *
      * @param traversible
      *        traversed {@link Traversible}
      */
@@ -84,7 +84,7 @@ extends AbstractIterator<Item> {
      * concrete {@link Traverser} in a property of the concrete type AND calling
      * THIS method using {@code super.setTraverser(delegateTraverser)}.
      * </p>
-     * 
+     *
      * @param delegateTraverser
      *        delegate {@link Traverser}
      */
