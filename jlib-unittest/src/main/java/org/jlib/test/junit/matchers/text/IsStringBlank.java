4cfa1825f9beae44373db080a39678f892e37ffd
Igor Akkerman <igor.akkerman@jlib.org>
2013-06-11 20:55:51 +0200

    IsStringBlank formatted

--- jlib-unittest/src/main/java/org/jlib/test/junit/matchers/text/IsStringBlank.java
+++ jlib-unittest/src/main/java/org/jlib/test/junit/matchers/text/IsStringBlank.java
@@ -6,16 +6,16 @@ import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.TypeSafeMatcher;
 
-
 public class IsStringBlank
 extends TypeSafeMatcher<String> {
+
     @Factory
     public static IsStringBlank blank() {
         return new IsStringBlank();
     }
 
     public IsStringBlank() {}
-    
+
     @Override
     public boolean matchesSafely(String string) {
         return StringUtils.isBlank(string);
@@ -25,5 +25,4 @@ extends TypeSafeMatcher<String> {
     public void describeTo(Description description) {
         description.appendText("blank string");
     }
-
 }
