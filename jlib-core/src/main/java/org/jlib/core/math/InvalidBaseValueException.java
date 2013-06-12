2856d3a2a9b418502053148dc96789ca41fd6aa1
Igor Akkerman <igor.akkerman@jlib.org>
2013-06-13 01:04:49 +0200

    MathUtility throwing InvalidBaseValueException

---
+++ jlib-core/src/main/java/org/jlib/core/math/InvalidBaseValueException.java
@@ -0,0 +1,25 @@
+package org.jlib.core.math;
+
+import org.jlib.core.IllegalJlibArgumentException;
+
+/**
+ * {@link IllegalJlibArgumentException} thrown when an invalid value has been specified
+ * as a base.
+ */
+public class InvalidBaseValueException
+extends IllegalJlibArgumentException {
+
+    private final int baseValue;
+
+    public InvalidBaseValueException(final int baseValue) {
+
+        // TODO: create constant for "{0}"
+        super("{0} < 1", baseValue);
+
+        this.baseValue = baseValue;
+    }
+
+    public int getBaseValue() {
+        return baseValue;
+    }
+}
