573a89f4101602d5f7ef65115e6e5c2acb3fff94
Igor Akkerman <igor.akkerman@jlib.org>
2013-06-13 00:31:23 +0200

    constructor removed

--- jlib-core/src/main/java/org/jlib/core/IllegalJlibArgumentException.java
+++ jlib-core/src/main/java/org/jlib/core/IllegalJlibArgumentException.java
@@ -35,15 +35,7 @@ import org.jlib.core.array.ArrayUtility;
 public abstract class IllegalJlibArgumentException
 extends IllegalArgumentException {
 
-    /** serialVersionUID */
     private static final long serialVersionUID = 5894034302749387338L;
-
-    /**
-     * Creates a new {@link IllegalJlibArgumentException}.
-     */
-    public IllegalJlibArgumentException() {
-        super();
-    }
 
     /**
      * Creates a new {@link IllegalJlibArgumentException}.
