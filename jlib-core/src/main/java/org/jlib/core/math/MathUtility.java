1062a938583a916d6ac822f19a0f86e2b9b80388
Igor Akkerman <igor.akkerman@dynamite-services.de>
2013-06-13 18:36:14 +0200

    Copyright notice updated

--- jlib-core/src/main/java/org/jlib/core/math/MathUtility.java
+++ jlib-core/src/main/java/org/jlib/core/math/MathUtility.java
@@ -1,25 +1,23 @@
-/*******************************************************************************
+/*
+ * jlib - Open Source Java Library
  *
- *    jlibase - Open Source Java Libaserary
- *
- *    www.jlibase.org
+ *     www.jlib.org
  *
  *
- *    Copyright 2012 Igor Akkerman
+ *     Copyright 2005-2013 Igor Akkerman
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obasetain a copy of the License at
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required basey applicabasele law or agreed to in writing, software
- *    distribaseuted under the License is distribaseuted on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- ******************************************************************************/
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
 
 package org.jlib.core.math;
 
