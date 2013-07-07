08d1a47f0b2a00173d9a99735c35843cff2cd50a
Igor Akkerman <igor.akkerman@dynamite-services.de>
2013-07-08 00:52:11 +0200

    Javadoc added

--- jlib-core/src/main/java/org/jlib/core/system/AbstractObject.java
+++ jlib-core/src/main/java/org/jlib/core/system/AbstractObject.java
@@ -21,6 +21,11 @@
 
 package org.jlib.core.system;
 
+import java.util.Collection;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import static org.apache.commons.lang3.ArrayUtils.EMPTY_STRING_ARRAY;
@@ -31,7 +36,7 @@ import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToStrin
 
 /**
  * Abstract {@link Object} implementing {@link #toString()}, {@link #equals(Object)} and {@link #hashCode()} using
- * the correspondent builders provided by
+ * the correspondent reflective builders provided by
  * <a href="http://commons.apache.org/proper/commons-lang/">Apache Commons Lang</a>.
  *
  * @author Igor Akkerman
@@ -47,27 +52,95 @@ public abstract class AbstractObject {
         super();
     }
 
+    /**
+     * <p>
+     * Returns a {@link String} providing a textual representation of this {@link AbstractObject}, according to the
+     * specification in {@link Object#toString()}.
+     * </p>
+     * <p>
+     * The implementation in the class {@link AbstractObject} uses
+     * {@link ToStringBuilder#reflectionToString(Object, ToStringStyle)} to build the {@link String} with the
+     * {@link ToStringStyle} specified by {@link #getToStringStyle()}.
+     * </p>
+     *
+     * @return {@link String} representation of this {@link AbstractObject}
+     */
     @Override
     public String toString() {
         return reflectionToString(this, getToStringStyle());
     }
 
+    /**
+     * <p>
+     * Returns the {@link ToStringStyle} used by {@link #toString()}.
+     * </p>
+     * <p>
+     * The implementation in the class {@link AbstractObject} returns the result of
+     * {@link ToStringBuilder#getDefaultStyle()}. It may be overridden to specify a more suitable {@link ToStringStyle}.
+     * An application-wide {@link ToStringStyle} may be registered using
+     * {@link ToStringBuilder#setDefaultStyle(ToStringStyle)}.
+     * </p>
+     *
+     * @return {@link ToStringStyle} used by {@link #toString()}
+     */
+    protected ToStringStyle getToStringStyle() {
+        return getDefaultStyle();
+    }
+
+    /**
+     * <p>
+     * Returns whether the specified {@link Object} is equal to this {@link AbstractObject}, according to the
+     * specification in {@link Object#equals(Object)}.
+     * </p>
+     * <p>
+     * The implementation in the class {@link AbstractObject} uses
+     * {@link EqualsBuilder#reflectionEquals(Object, Object, Collection)} to perform the verification.
+     * The fields provided by {@link #getExcludedFields()} are excluded from the verification.
+     * </p>
+     *
+     * @param otherObject
+     *        {@link Object} to be compared for equality to this {@link AbstractObject}
+     *
+     * @return {@code true} if {@code otherObject} is equal to this {@link AbstractObject};
+     *         {@code false} otherwise
+     */
     @Override
     @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
     public boolean equals(final Object otherObject) {
         return reflectionEquals(this, otherObject, getExcludedFields());
     }
 
+    /**
+     * <p>
+     * Returns a hash code value for this {@link AbstractObject}, according to the specification in
+     * {@link Object#hashCode()}.
+     * </p>
+     * <p>
+     * The implementation in the class {@link AbstractObject} uses
+     * {@link HashCodeBuilder#reflectionHashCode(Object, String...)} to perform the verification.
+     * The fields provided by {@link #getExcludedFields()} are excluded from the verification.
+     * </p>
+     *
+     * @return integer specifying the hash code
+     */
     @Override
     public int hashCode() {
         return reflectionHashCode(this, getExcludedFields());
     }
 
+    /**
+     * <p>
+     * Reuturns the names of the fields excluded from the operations in {@link #equals(Object)} and {@link #hashCode()}.
+     * </p>
+     * <p>
+     * The implementation in the class {@link AbstractObject} returns an empty array. It may be overridden to specify
+     * a set of excluded fields.
+     * </p>
+     *
+     *
+     * @return array of {@link String}s specifying the names of the excluded fields
+     */
     protected String[] getExcludedFields() {
         return EMPTY_STRING_ARRAY;
-    }
-
-    protected ToStringStyle getToStringStyle() {
-        return getDefaultStyle();
     }
 }
