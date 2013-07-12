68ccaa3849cc87921353f0f76c03534afcf3f765
Igor Akkerman <igor.akkerman@dynamite-services.de>
2013-07-12 17:35:27 +0200

    ValueHolder framework reworked

---
+++ jlib-core/src/main/java/org/jlib/core/accessor/Accessor.java
@@ -0,0 +1,54 @@
+/*
+ * jlib - Open Source Java Library
+ *
+ *     www.jlib.org
+ *
+ *
+ *     Copyright 2005-2013 Igor Akkerman
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package org.jlib.core.accessor;
+
+import org.jlib.core.language.ValueNotAccessibleException;
+
+/**
+ * Accessor of a {@link Value}.
+ *
+ * @param <Value>
+ *        type of the value
+ *
+ * @author Igor Akkerman
+ */
+public interface Accessor<Value> {
+
+    /**
+     * Returns the accessed {@link Value}.
+     *
+     * @return accessed {@link Value}
+     *
+     * @throws ValueNotAccessibleException
+     *         if no {@link Value} can be accessed
+     */
+    public Value getValue()
+    throws ValueNotAccessibleException;
+
+    /**
+     * Returns whether a {@link Value} can be accessed.
+     *
+     * @return {@code true} if a {@link Value} can be accessed;
+     *         {@code false} otherwise
+     */
+    public boolean isValueAccessible();
+}
---
+++ jlib-core/src/main/java/org/jlib/core/operator/InitializedOperated.java
@@ -0,0 +1,52 @@
+/*
+ * jlib - Open Source Java Library
+ *
+ *     www.jlib.org
+ *
+ *
+ *     Copyright 2005-2013 Igor Akkerman
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package org.jlib.core.operator;
+
+import org.jlib.core.accessor.Initialized;
+
+/**
+ * {@link Initialized} and {@link OperatedAccessor}.
+ *
+ * @param <Value>
+ *        type of the value used by the {@link OptionalValueOperator}
+ *
+ * @author Igor Akkerman
+ */
+class InitializedOperated<Value>
+extends Initialized<Value>
+implements OperatedAccessor<Value> {
+
+    /**
+     * Creates a new {@link InitializedOperated}.
+     *
+     * @param initialValue
+     *        initial Value
+     */
+    public InitializedOperated(final Value initialValue) {
+        super(initialValue);
+    }
+
+    @Override
+    public final void operate(final OptionalValueOperator<Value> operator) {
+        operator.operate(getValue());
+    }
+}
