8aa070ecb305cbbe2bce206f698f008505246bab
Igor Akkerman <igor.akkerman@jlib.org>
2013-07-17 23:51:42 +0200

    Test modified according to refactorings

--- jlib-core/src/test/java/org/jlib/core/collection/CachingMapTest.java
+++ jlib-core/src/test/java/org/jlib/core/collection/CachingMapTest.java
@@ -3,6 +3,7 @@ package org.jlib.core.collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.collect.ForwardingMap;
 import org.junit.Test;
 
 public class CachingMapTest {
@@ -14,15 +15,19 @@ public class CachingMapTest {
         hashMap.put("gut", "schlecht");
         hashMap.put("München", "Berlin");
 
-        final Map<String, String> delegatingMap = new DelegatingMap<String, String>(hashMap) {
-            // intentilonally empty
+        final Map<String, String> forwardingMap = new ForwardingMap<String, String>() {
+
+            @Override
+            protected Map<String, String> delegate() {
+                return hashMap;
+            }
         };
 
         final Map<String, String> cacheMap = new CachingMap<>(hashMap);
 
         for (int i = 0; i < 20; i++) {
             System.out.println("HashMap:       " + measure(hashMap));
-            System.out.println("DelegatingMap: " + measure(delegatingMap));
+            System.out.println("DelegatingMap: " + measure(forwardingMap));
             System.out.println("CacheMap:      " + measure(cacheMap));
         }
     }
