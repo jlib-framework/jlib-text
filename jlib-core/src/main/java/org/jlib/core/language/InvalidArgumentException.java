03c1c2bb01986d66ea5397979ceaf177bb1d2605
Igor Akkerman <igor.akkerman@jlib.org>
2013-07-20 11:17:41 +0200

    Detailed Javadoc added, createMessage made protected

--- jlib-core/src/main/java/org/jlib/core/language/InvalidArgumentException.java
+++ jlib-core/src/main/java/org/jlib/core/language/InvalidArgumentException.java
@@ -95,7 +95,14 @@ extends IllegalArgumentException {
     }
 
     /**
+     * <p>
      * Creates the message applying the specified message arguments to the specified message template.
+     * </p>
+     * <p>
+     * The implementation in {@link InvalidArgumentException} simply calls
+     * {@link ExceptionUtility#formatMessage(TextFormatter, CharSequence, Object...)}.
+     * Subclasses may alter the default behaviour.
+     * </p>
      *
      * @param messageTemplate
      *        {@link CharSequence} specifying the message template
@@ -103,7 +110,7 @@ extends IllegalArgumentException {
      * @param messageArguments
      *        comma separated sequence of {@link Object} message arguments
      */
-    private String createMessage(final CharSequence messageTemplate, final Object... messageArguments) {
+    protected String createMessage(final CharSequence messageTemplate, final Object... messageArguments) {
         return formatMessage(getMessageFormatter(), messageTemplate, messageArguments);
     }
 
