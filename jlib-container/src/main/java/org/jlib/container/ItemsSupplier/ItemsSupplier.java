12a0b317dc5c8004f38bf0ca61958f9f3f8b4d84
Igor Akkerman <igor.akkerman@dynamite-services.de>
2013-07-26 13:50:03 +0200

    building context

--- jlib-container/src/main/java/org/jlib/container/ItemsSupplier/ItemsSupplier.java
+++ jlib-container/src/main/java/org/jlib/container/ItemsSupplier/ItemsSupplier.java
@@ -1,6 +1,19 @@
-package org.jlib.container.ItemsSupplier;
+package org.jlib.container.itemssupplier;
+
+import java.util.Iterator;
+
+import org.jlib.core.traverser.InvalidTraversableStateException;
 
 public interface ItemsSupplier<Item> {
 
-    <Result> Result accept(ItemsSupplierVisitor<Item, Result> visitor);
+    public boolean contains(Item item)
+    throws InvalidTraversableStateException;
+
+    public boolean contains(Iterable<Item> items)
+    throws InvalidTraversableStateException;
+
+    public Iterator<Item> createIterator()
+    throws InvalidTraversableStateException;
+
+    public <Result> Result accept(ItemsSupplierVisitor<Item, Result> visitor);
 }
