3a00d97bd99b6fb1d5e97b42b8af28f75fc6fdfc
Igor Akkerman <igor.akkerman@jlib.org>
2013-07-27 14:52:42 +0200

    Working on Traversers

--- jlib-core/src/main/java/org/jlib/core/traverser/StuckTraverserState.java
+++ jlib-core/src/main/java/org/jlib/core/traverser/StuckTraverserState.java
@@ -21,26 +21,43 @@
 
 package org.jlib.core.traverser;
 
-public abstract class FirstItemTraverserState<Item>
-extends AbstractTraverserState<Item> {
+public final class StuckTraverserState<Item, Travble extends Traversable<Item>, State extends TwoWayTraverserState<Item, State>>
+extends TraversableAware<Item, Travble>
+implements TwoWayTraverserState<Item, State> {
 
-    public FirstItemTraverserState(final Traversable<Item> traversable) {
+    public StuckTraverserState(final Travble traversable) {
         super(traversable);
     }
 
     @Override
-    public final boolean hasPreviousItem() {
+    public boolean hasPreviousItem() {
         return false;
     }
 
     @Override
-    public final Item getPreviousItem()
+    public Item getPreviousItem()
     throws NoPreviousItemException {
         throw new NoPreviousItemException(getTraversable());
     }
 
     @Override
-    public final TwoWayTraverserState<Item, FirstItemTraverserState<Item>> getPreviousState() {
+    public State getPreviousState() {
         throw new NoPreviousItemException(getTraversable());
+    }
+
+    @Override
+    public boolean hasNextItem() {
+        return false;
+    }
+
+    @Override
+    public Item getNextItem()
+    throws NoNextItemException {
+        throw new NoNextItemException(getTraversable());
+    }
+
+    @Override
+    public State getNextState() {
+        throw new NoNextItemException(getTraversable());
     }
 }
