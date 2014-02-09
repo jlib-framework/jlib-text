331368e7019bb10d9d342319717ec3a0907fb991
Igor Akkerman <igor.akkerman@jlib.org>
2014-02-09 22:41:50 +0100

    CachingMap and unit test optimized

--- jlib-core/src/test/java/org/jlib/core/collection/CachingMapTest.java
+++ jlib-core/src/test/java/org/jlib/core/collection/CachingMapTest.java
@@ -13,7 +13,7 @@ public class CachingMapTest {
         final Map<String, String> hashMap = new HashMap<>();
         hashMap.put("ja", "nein");
         hashMap.put("gut", "schlecht");
-        hashMap.put("München", "Berlin");
+        hashMap.put("Münchenhausen", "Berlin");
 
         final Map<String, String> forwardingMap = new ForwardingMap<String, String>() {
 
@@ -27,20 +27,86 @@ public class CachingMapTest {
 
         for (int i = 0; i < 20; i++) {
             System.out.println("HashMap:       " + measure(hashMap));
-            System.out.println("DelegatingMap: " + measure(forwardingMap));
+            System.out.println("ForwardingMap: " + measure(forwardingMap));
             System.out.println("CacheMap:      " + measure(cacheMap));
+            System.out.println();
+        }
+    }
+
+    //    @Test
+    public void performanceDirect() {
+
+        final int MAX = 1000000;
+        final String MUC = "Münchenhausen";
+
+        final Map<String, String> map = new HashMap<>();
+        map.put("ja", "nein");
+        map.put("gut", "schlecht");
+        map.put(MUC, "Berlin");
+
+        for (int j = 0; j < 20; j++) {
+
+            long start = System.nanoTime();
+
+            for (int i = 0; i < MAX; i++)
+                map.containsKey(MUC);
+
+            long end = System.nanoTime();
+
+            System.out.println(end - start);
+
+            start = System.nanoTime();
+
+            for (int i = 0; i < MAX; i++)
+                map.get(MUC);
+
+            end = System.nanoTime();
+
+            System.out.println(end - start);
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void performanceCachedGetOnly() {
+
+        final int MAX = 1000000;
+        final String MUC = "Münchenhausen";
+
+        final Map<String, String> map = new CachingMap<>(new HashMap<String, String>());
+        map.put("ja", "nein");
+        map.put("gut", "schlecht");
+        final String BERLIN = "Berlin";
+        map.put(MUC, BERLIN);
+
+        map.containsKey(MUC);
+
+        for (int j = 0; j < 20; j++) {
+
+            System.gc();
+            final long start = System.nanoTime();
+
+            for (int i = 0; i < MAX; i++)
+                if (map.get(MUC) != BERLIN)
+                    System.out.println("x");
+
+            final long end = System.nanoTime();
+
+            System.out.println("" + (end - start) + " [" + end + " - " + start + "]");
         }
     }
 
     private long measure(final Map<String, String> map) {
-        final int MAX = 100;
-        final String MUC = "ja";
+        final int MAX = 1000000;
+        final String MUC = "Münchenhausen";
 
         long start = System.nanoTime();
 
-        for (int i = 0; i < MAX; i++)
+        for (int i = 0; i < MAX; i++) {
+            System.gc();
             if (map.containsKey(MUC))
                 map.get(MUC);
+        }
 
         long end = System.nanoTime();
 
