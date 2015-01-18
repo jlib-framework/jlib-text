5ba70e2664ee5b0eabed0ff156bc57b3b301e882
Igor Akkerman <igor.akkerman@jlib.org>
2015-01-18 12:53:23 +0100

    MessageFormatNamedValueFormatter with unit test methods created

--- jlib-core/src/test/java/org/jlib/core/text/ParametrizedMessageTest.java
+++ jlib-core/src/test/java/org/jlib/core/text/ParametrizedMessageTest.java
@@ -29,27 +29,36 @@ import org.junit.Test;
 
 public class ParametrizedMessageTest {
 
-    private static final ParametrizedMessageConfiguration COLON_CONFIGURATION;
+    private static final ParametrizedMessageConfiguration COLON_PRINTF_CONFIG;
 
     static {
-        COLON_CONFIGURATION = new ParametrizedMessageConfiguration();
-        COLON_CONFIGURATION.setArgumentFormatter(new PrintfNamedValueFormatter("%s: %s"));
-        COLON_CONFIGURATION.setTextArgumentsSeparator(" ");
-        COLON_CONFIGURATION.setArgumentsSeparator("; ");
+        COLON_PRINTF_CONFIG = new ParametrizedMessageConfiguration();
+        COLON_PRINTF_CONFIG.setArgumentFormatter(new PrintfNamedValueFormatter("%s: %s"));
+        COLON_PRINTF_CONFIG.setTextArgumentsSeparator(" ");
+        COLON_PRINTF_CONFIG.setArgumentsSeparator("; ");
     }
 
-    private static final ParametrizedMessageConfiguration EQUALS_SINGLE_QUOTE_CONFIGURATION;
+    private static final ParametrizedMessageConfiguration EQUALS_QUOTE_PRINTF_CONFIG;
 
     static {
-        EQUALS_SINGLE_QUOTE_CONFIGURATION = new ParametrizedMessageConfiguration();
-        EQUALS_SINGLE_QUOTE_CONFIGURATION.setArgumentFormatter(new PrintfNamedValueFormatter("%s='%s'"));
-        EQUALS_SINGLE_QUOTE_CONFIGURATION.setTextArgumentsSeparator(" ");
-        EQUALS_SINGLE_QUOTE_CONFIGURATION.setArgumentsSeparator(" ");
+        EQUALS_QUOTE_PRINTF_CONFIG = new ParametrizedMessageConfiguration();
+        EQUALS_QUOTE_PRINTF_CONFIG.setArgumentFormatter(new PrintfNamedValueFormatter("%s='%s'"));
+        EQUALS_QUOTE_PRINTF_CONFIG.setTextArgumentsSeparator(" ");
+        EQUALS_QUOTE_PRINTF_CONFIG.setArgumentsSeparator(" ");
+    }
+
+    private static final ParametrizedMessageConfiguration COLON_MF_CONFIG;
+
+    static {
+        COLON_MF_CONFIG = new ParametrizedMessageConfiguration();
+        COLON_MF_CONFIG.setArgumentFormatter(new MessageFormatNamedValueFormatter("{0}: {1}"));
+        COLON_MF_CONFIG.setTextArgumentsSeparator(" ");
+        COLON_MF_CONFIG.setArgumentsSeparator("; ");
     }
 
     @Before
     public void initializeDefaultConfiguration() {
-        ParametrizedMessageFactory.getInstance().setDefaultConfiguration(EQUALS_SINGLE_QUOTE_CONFIGURATION);
+        ParametrizedMessageFactory.getInstance().setDefaultConfiguration(EQUALS_QUOTE_PRINTF_CONFIG);
     }
 
     @Test
@@ -83,27 +92,35 @@ public class ParametrizedMessageTest {
 
     @Test
     public void thrownExceptionShouldHaveTextMessageWithNamedArgumentsInSpecifiedDefaultFormat() {
-        ParametrizedMessageFactory.getInstance().setDefaultConfiguration(COLON_CONFIGURATION);
+        ParametrizedMessageFactory.getInstance().setDefaultConfiguration(COLON_PRINTF_CONFIG);
 
-        final ParametrizedMessage message = message("Something went wrong.").with(named("dummyName", 1),
-                                                                                  named("dummerName", "Dummer Value"));
+        final ParametrizedMessage message = /*
+         */ message("Something went wrong.").with(named("dummyName", 1), named("dummerName", "Dummer Value"));
 
         assertThat(message.toString()).isEqualTo("Something went wrong. dummyName: 1; dummerName: Dummer Value");
     }
 
     @Test
     public void thrownExceptionShouldHaveTextMessageWithNamedArguments() {
-        final ParametrizedMessage message = message("Something went wrong.").with(named("dummyName", 1),
-                                                                                  named("dummerName", "Dummer Value"));
+        final ParametrizedMessage message = /*
+         */ message("Something went wrong.").with(named("dummyName", 1), named("dummerName", "Dummer Value"));
 
         assertThat(message.toString()).isEqualTo("Something went wrong. dummyName='1' dummerName='Dummer Value'");
     }
 
     @Test
-    public void thrownExceptionShouldHaveTextMessageWithArgumentsInSpecifiedFormat() {
-        final ParametrizedMessage message = message("Something went wrong.", COLON_CONFIGURATION).with("dummyName", 1)
-                                                                                                 .with("dummerName",
-                                                                                                       "Dummer Value");
+    public void thrownExceptionShouldHaveTextMessageWithArgumentsInSpecifiedPrintfFormat() {
+        final ParametrizedMessage message = /*
+         */ message("Something went wrong.", COLON_PRINTF_CONFIG).with("dummyName", 1)
+                                                                 .with("dummerName", "Dummer Value");
+
+        assertThat(message.toString()).isEqualTo("Something went wrong. dummyName: 1; dummerName: Dummer Value");
+    }
+
+    @Test
+    public void thrownExceptionShouldHaveTextMessageWithArgumentsInSpecifiedMfFormat() {
+        final ParametrizedMessage message = /*
+          */ message("Something went wrong.", COLON_MF_CONFIG).with("dummyName", 1).with("dummerName", "Dummer Value");
 
         assertThat(message.toString()).isEqualTo("Something went wrong. dummyName: 1; dummerName: Dummer Value");
     }
