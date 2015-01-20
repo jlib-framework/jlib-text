4f5a927032d6016ffdfb3e6a98659e956ae20123
Igor Akkerman <igor.akkerman@jlib.org>
2015-01-20 17:40:01 +0100

    Reworking Checkstyle usage

--- jlib-text/src/test/java/org/jlib/text/transformer/LeftAligningStringTransformerTest.java
+++ jlib-text/src/test/java/org/jlib/text/transformer/LeftAligningStringTransformerTest.java
@@ -6,9 +6,6 @@ import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
-
 /**
  * Test case for LeftAligningStringTransformer.
  *
